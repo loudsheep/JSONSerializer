cb466c5ce726dca55c36d8ef81df8d857f943c5d
loudsheep <rochmykietow@gmail.com>
2021-03-22 10:00:51 +0100

    Added javadoc to JSONReader

--- src/json/JSONReader.java
+++ src/json/JSONReader.java
@@ -3,6 +3,10 @@ package json;
 import json.dataTypes.JSONData;
 
 public class JSONReader {
+    /**
+     * @param text source text containing json data
+     * @return JSONData object
+     */
     public static JSONData toJSONObject(String text) {
         return new Parser(new Scanner(text).scanTokens()).parse();
     }
